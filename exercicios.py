d41f2240b2415cf5f15a236c89ef73725da67a9a
vgbarbosa <valmon.dasilvabarbosa@cencosud.com.br>
2024-10-02 21:40:36 -0300

    salvando meus exercicios de python

---
+++ exercicios.py
@@ -0,0 +1,18 @@
+# Exercício 1: Soma de Dois Números Inteiros
+#num1 = int(input("Digite o primeiro número inteiro:"))
+#num2 = int(input("Digite o segundo número inteiro:"))
+#print(f"O resultado da soma é:{num1+num2}")
+
+# Exercício 2: Resto da Divisão por 5
+#num = int(input("Digite um número:"))
+#print(f"O resto da divisão por 5 é: {num %5}")
+
+# Exercício 3: Multiplicação de Dois Números
+#num1 = int(input("Digite o primeiro número inteiro:"))
+#num2 = int(input("Digite o segundo número inteiro:"))
+#print(f"O resultado da multiplicação dos dois números é:{num1*num2}")
+
+# Exercício 4: Divisão Inteira do Primeiro pelo Segundo Número
+num1 = int(input("Digite um número:"))
+num2 = int(input("Digite o segundo número:"))
+print(f"O resultado da divisão é: {num1//num2}")
