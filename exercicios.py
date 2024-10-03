9514ac56ebeb5374e61219f659e01ad2601498c8
vgbarbosa <valmon.dasilvabarbosa@cencosud.com.br>
2024-10-02 22:12:18 -0300

    atualizando estrututa de visualização down-top

--- exercicios.py
+++ exercicios.py
@@ -1,35 +1,6 @@
-# Exercício 1: Soma de Dois Números Inteiros
-#num1 = int(input("Digite o primeiro número inteiro:"))
-#num2 = int(input("Digite o segundo número inteiro:"))
-#print(f"O resultado da soma é:{num1+num2}")
-
-# Exercício 2: Resto da Divisão por 5
-#num = int(input("Digite um número:"))
-#print(f"O resto da divisão por 5 é: {num %5}")
-
-# Exercício 3: Multiplicação de Dois Números
-#num1 = int(input("Digite o primeiro número inteiro:"))
-#num2 = int(input("Digite o segundo número inteiro:"))
-#print(f"O resultado da multiplicação dos dois números é:{num1*num2}")
-
-# Exercício 4: Divisão Inteira do Primeiro pelo Segundo Número
-#num1 = int(input("Digite um número:"))
-#num2 = int(input("Digite o segundo número:"))
-#print(f"O resultado da divisão é: {num1//num2}")
-
-# Exercício 5: Quadrado de um Número
-#num = int(input("Digite um número:"))
-#print(f"O quadrado do número é : {num**2}")
-
-# Exercício 6: Adição de Dois Números Flutuantes
-#num1 = float(input("Digite o primeiro flutuante:"))
-#num2 = float(input("Digite o segundo número flutuante:"))
-#print(f"A soma é:{num1+num2}")
-
-# Exercício 7: Média de Dois Números Flutuantes
-#num1 = float(input("Digite o primeiro flutuante:"))
-#num2 = float(input("Digite o segundo número flutuante:"))
-#print(f"A média é:{(num1+num2)/2}")
+# Exercício 9: Conversão de Celsius para Fahrenheit
+celsius = float(input("Digite a temperatuca em Celsius: "))
+print(f"{celsius}°C é igual a {(celsius*9/5)+32}")  
 
 # Exercício 8: Potência de um Número
 #base = float(input("Digite a base:"))
@@ -37,6 +8,41 @@
 #potencia = base ** expoente
 #print("O resultado da potência é:", potencia)
 
-# Exercício 9: Conversão de Celsius para Fahrenheit
-celsius = float(input("Digite a temperatuca em Celsius: "))
-print(f"{celsius}°C é igual a {(celsius*9/5)+32}")  
+# Exercício 7: Média de Dois Números Flutuantes
+#num1 = float(input("Digite o primeiro flutuante:"))
+#num2 = float(input("Digite o segundo número flutuante:"))
+#print(f"A média é:{(num1+num2)/2}")
+
+# Exercício 6: Adição de Dois Números Flutuantes
+#num1 = float(input("Digite o primeiro flutuante:"))
+#num2 = float(input("Digite o segundo número flutuante:"))
+#print(f"A soma é:{num1+num2}")
+
+# Exercício 5: Quadrado de um Número
+#num = int(input("Digite um número:"))
+#print(f"O quadrado do número é : {num**2}")
+
+# Exercício 4: Divisão Inteira do Primeiro pelo Segundo Número
+#num1 = int(input("Digite um número:"))
+#num2 = int(input("Digite o segundo número:"))
+#print(f"O resultado da divisão é: {num1//num2}")
+
+# Exercício 3: Multiplicação de Dois Números
+#num1 = int(input("Digite o primeiro número inteiro:"))
+#num2 = int(input("Digite o segundo número inteiro:"))
+#print(f"O resultado da multiplicação dos dois números é:{num1*num2}")
+
+# Exercício 2: Resto da Divisão por 5
+#num = int(input("Digite um número:"))
+#print(f"O resto da divisão por 5 é: {num %5}")
+
+# Exercício 1: Soma de Dois Números Inteiros
+#num1 = int(input("Digite o primeiro número inteiro:"))
+#num2 = int(input("Digite o segundo número inteiro:"))
+#print(f"O resultado da soma é:{num1+num2}")
+
+
+
+
+
+
