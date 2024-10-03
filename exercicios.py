c215f0d957972f634b970004b3872e6cb60ea21a
vgbarbosa <valmon.dasilvabarbosa@cencosud.com.br>
2024-10-02 22:08:17 -0300

    atualizando minha lista de exercicios em python

--- exercicios.py
+++ exercicios.py
@@ -13,6 +13,30 @@
 #print(f"O resultado da multiplicação dos dois números é:{num1*num2}")
 
 # Exercício 4: Divisão Inteira do Primeiro pelo Segundo Número
-num1 = int(input("Digite um número:"))
-num2 = int(input("Digite o segundo número:"))
-print(f"O resultado da divisão é: {num1//num2}")
+#num1 = int(input("Digite um número:"))
+#num2 = int(input("Digite o segundo número:"))
+#print(f"O resultado da divisão é: {num1//num2}")
+
+# Exercício 5: Quadrado de um Número
+#num = int(input("Digite um número:"))
+#print(f"O quadrado do número é : {num**2}")
+
+# Exercício 6: Adição de Dois Números Flutuantes
+#num1 = float(input("Digite o primeiro flutuante:"))
+#num2 = float(input("Digite o segundo número flutuante:"))
+#print(f"A soma é:{num1+num2}")
+
+# Exercício 7: Média de Dois Números Flutuantes
+#num1 = float(input("Digite o primeiro flutuante:"))
+#num2 = float(input("Digite o segundo número flutuante:"))
+#print(f"A média é:{(num1+num2)/2}")
+
+# Exercício 8: Potência de um Número
+#base = float(input("Digite a base:"))
+#expoente = float(input("Digite o expoente:"))
+#potencia = base ** expoente
+#print("O resultado da potência é:", potencia)
+
+# Exercício 9: Conversão de Celsius para Fahrenheit
+celsius = float(input("Digite a temperatuca em Celsius: "))
+print(f"{celsius}°C é igual a {(celsius*9/5)+32}")  
