6d491d07f3ad6c35f46863ceaf20a1ed03e9f5c1
vgbarbosa <valmon.dasilvabarbosa@cencosud.com.br>
2024-10-11 00:29:36 -0300

    insert layout desafio

--- desafio.py
+++ desafio.py
@@ -1,3 +1,7 @@
+### Desafio - Refatorar o projeto da aula anterior evitando Bugs!
+
+# 1) Solicita ao usuário que digite seu nome
+
 nome_usuario = input("Digite seu nome de usuário: ")
 
 if nome_usuario.isdigit():
@@ -9,3 +13,15 @@ elif len(nome_usuario) == 0:
 elif nome_usuario.isspace():
     print("Você digitou apenas espaço")
     exit()
+
+# 2) Solicita ao usuário que digite o valor do seu salário
+# Converte a entrada para um número de ponto flutuante
+
+# 3) Solicita ao usuário que digite o valor do bônus recebido
+# Converte a entrada para um número de ponto flutuante
+
+# 4) Calcule o valor do bônus final
+
+# 5) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
+
+# Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
