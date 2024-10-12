2b5e41128fea8aaa0013762c169011c5be1b89cf
vgbarbosa <valmon.dasilvabarbosa@cencosud.com.br>
2024-10-11 23:26:44 -0300

    Atualizando a task 2

--- desafio.py
+++ desafio.py
@@ -5,7 +5,7 @@
 nome_usuario = input("Digite seu nome de usuário: ")
 
 if nome_usuario.isdigit():
-    print("Você digitou seunome errado")
+    print("Você digitou seu nome errado")
     exit()
 elif len(nome_usuario) == 0:
     print("Você não digitou nada")
@@ -17,10 +17,16 @@ elif nome_usuario.isspace():
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
 
-print(float(input("Qual o valor de seu salário?")))
-
+#print(float(input("Qual o valor de seu salário?")))
+salario = float(input("Qual o valor de seu salário?"))
+print(salario)
+if isinstance(salario, float):
+    print("Valor válido")
+    exit()
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
+
+
 
 # 4) Calcule o valor do bônus final
 
